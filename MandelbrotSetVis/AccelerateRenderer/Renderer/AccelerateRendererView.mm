d2c13bde2bd2d9c9440d29059fb789b8c525b4ee
Denis <nesseratious@gmail.com>
2021-02-13 17:19:33 +0200

    Created AccelerateRendererView

---
+++ MandelbrotSetVis/AccelerateRenderer/Renderer/AccelerateRendererView.mm
@@ -0,0 +1,20 @@
+//
+//  AccelerateRendererView.m
+//  MandelbrotSetVis
+//
+//  Created by Esie on 2/13/21.
+//
+
+#import "AccelerateRendererView.h"
+
+@implementation AccelerateRendererView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
