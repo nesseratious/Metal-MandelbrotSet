e5e884dd0b2fcc6bf9057b4d1fd43dbcd77e6bc7
Denis <nesseratious@gmail.com>
2021-03-02 08:24:47 +0200

    Add comments

--- MandelbrotSetVis/ViewControllers/MandelbrotViewController.swift
+++ MandelbrotSetVis/ViewControllers/MandelbrotViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 final class MandelbrotViewController: UIViewController {
     private let renderer: Renderer
+    
+    /// Holds this view controller's transformation of the mandelbrot set.
     private var transform = SceneTransform()
     
     init(renderer: Renderer) {
@@ -36,6 +38,7 @@ final class MandelbrotViewController: UIViewController {
         calculateAspectRatio()
     }
     
+    /// Adds gestures to pan and zoom mandelbrot view.
     private func addGestures() {
         let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(zoom(sender:)))
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pan(sender:)))
@@ -44,41 +47,51 @@ final class MandelbrotViewController: UIViewController {
         renderer.addGestureRecognizer(panGesture)
     }
     
+    /// Calculates aspect ratio of the first frame and writes it to the renderer's bridge buffer.
+    /// Runs only once when the vc is first opened.
     private func calculateAspectRatio() {
         let scaleMultiplier: Float = 1.15
-        let widht = Float(view.frame.width)
+        let width = Float(view.frame.width)
         let height = Float(view.frame.height)
         renderer.buffer.aspectRatio.x = scaleMultiplier
-        renderer.buffer.aspectRatio.y = height / widht * scaleMultiplier
+        renderer.buffer.aspectRatio.y = height / width * scaleMultiplier
     }
 }
 
-private extension MandelbrotViewController {
-    @objc func zoom(sender: UIPinchGestureRecognizer) {
+@objc private extension MandelbrotViewController {
+    func pan(sender: UIPanGestureRecognizer) {
         guard sender.view != nil else { return }
-        let scale = transform.zoom * Float(sender.scale)
-        switch sender.state {
-        case .began, .changed:
-            renderer.buffer.scale = 1.0 / scale
-        case .ended:
-            transform.zoom = scale
-        default:
-            break
-        }
-    }
-
-    @objc func pan(sender: UIPanGestureRecognizer) {
-        guard sender.view != nil else { return }
-        let deltaX = Float(sender.translation(in: view).x/view.frame.width) * Float(view.frame.width / 200)
-        let deltaY = Float(sender.translation(in: view).y/view.frame.height) * Float(view.frame.height / 200)
-        let shiftX = transform.x + deltaX / transform.zoom
-        let shiftY = transform.y - deltaY / transform.zoom
+        
+        /// The amount of pixels of the mandelbrot that should corespond to one point in gesture's translation.
+        /// Pixel is one mandelbrot unit of calculation (doesn't reflect actual pixels on screen), point is UIKit point.
+        /// Modifing this value will change sensitivity of the pan gesture.
+        /// The default is 1 point = 175 pixels.
+        let pixelsPerPoint: CGFloat = 175
+        
+        let deltaX = Float(sender.translation(in: view).x / view.frame.width)
+        let deltaY = Float(sender.translation(in: view).y / view.frame.height)
+        let shiftX = transform.x + deltaX * Float(view.frame.width / pixelsPerPoint) / transform.zoom
+        let shiftY = transform.y - deltaY * Float(view.frame.height / pixelsPerPoint) / transform.zoom
         switch sender.state {
         case .began, .changed:
             renderer.buffer.translation = (shiftX, shiftY)
         case .ended:
             transform.x = shiftX
             transform.y = shiftY
+        default:
+            break
+        }
+    }
+    
+     func zoom(sender: UIPinchGestureRecognizer) {
+        guard sender.view != nil else { return }
+        
+        let scale = transform.zoom * Float(sender.scale)
+        switch sender.state {
+        case .began, .changed:
+            renderer.buffer.scale = 1.0 / scale
+        case .ended:
+            transform.zoom = scale
         default:
             break
         }
